650d9a98c63321455541639c92c7ea59258b1de5
castor <castord.d@gmail.com>
2018-08-07 23:28:02 -0300

    Entidades y Relaciones

--- app/Comment.php
+++ app/Comment.php
@@ -4,13 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Video extends Model
-{
-    protected $table='videos';
-
-    public function comments(){
-    	return $this->hasMany('App\Comment');
-    }
+class Comment extends Model
+{	
+	protected $table='comments';
 
     public function user(){
     	return $this->belongsTo('App\user','user_id');
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Video;
 
 Route::get('/', function () {
+	$videos = video::all();
+	foreach($videos as $video){
+		echo "<h2>$video->title</h2>";
+		echo "<p>$video->user->email</p>";
+		//var_dump($video);
+		foreach ($video->comments as $coment) {
+			echo "<p>$coment->body</p>";
+		}
+		
+	}
+	die();
     return view('welcome');
 });
